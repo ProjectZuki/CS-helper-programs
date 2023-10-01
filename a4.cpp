7d3578bdf7f8fccfe7546ea72d83544b5504f1da
Willie Alcaraz <63901327+ProjectZuki@users.noreply.github.com>
2023-09-30 18:57:49 -0700

    Added helper program for cs477 a4

---
+++ a4.cpp
@@ -0,0 +1,23 @@
+/// @brief This program calculates the sum of the first n terms of the series
+/// using the recurrence relation
+/// t_n = (n-1) + (2/n) * sum
+
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double n = 1000000.0;
+  double sum = 0.0;
+  double t_n = 0.0;
+
+  for (double i = 2; i <= n; i++) {
+    t_n = i - 1 + (2 / i) * sum;
+    if (static_cast<int>(i) % 10000 == 0) {
+        cout << "n: " << i << " t_n: " << setprecision(2) << fixed << t_n << endl;
+    }
+    sum += t_n;
+  }
+  return 0;
+}
